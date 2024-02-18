d7e5bc863aaecbb0760ccd330ae0bde8be4172cb
Zoltán Balázs <git@zoltanbalazs.com>
2024-02-18 17:12:38 +0100

    Added 2nd lesson base

---
+++ 2023-24-02/02/Circle_Point/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using TextFile;
+using System.Collections.Generic;
+
+namespace Circle_Point
+{
+    class Program
+    {
+        static void Main()
+        {
+            try {
+                TextFileReader reader = new ("input.txt");
+
+                reader.ReadDouble(out double a);
+                reader.ReadDouble(out double b);
+                reader.ReadDouble(out double c);
+                Circle k = new (new Point(a, b), c);
+
+                reader.ReadInt(out int n);
+                Point[] x = new Point[n];
+                for (int i = 0; i<n; ++i)
+                {
+                    reader.ReadDouble(out a);
+                    reader.ReadDouble(out b);
+                    x[i] = new Point(a, b);
+                }
+
+                // számlálás
+                int db = 0;
+                foreach (Point e in x)
+                {
+                    if (k.Contains(e)) ++db;
+                }
+                Console.WriteLine($"A kör lemezére eső pontok száma: {db}" );
+
+            }
+            catch(System.IO.FileNotFoundException)
+            {
+                Console.WriteLine("Rossz a fájl név!");
+            }
+            catch (Circle.WrongRadiusException)
+            {
+                Console.WriteLine("Kör sugara nem lehet negativ!");
+            }
+        }
+
+    }
+}
---
+++ 2023-24-02/02/Complex/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Globalization;
+
+namespace Complex
+{
+    class Program
+    {
+        static void Main()
+        {
+            Thread.CurrentThread.CurrentCulture = new CultureInfo("en-US");
+            try
+            {
+                Complex a = new(-3.0, 2.0);
+                Complex b = new(-1.3, 2.5);
+
+                Console.WriteLine($"a = {a}, b = {b}");
+
+                Console.WriteLine($"a + b = {a + b}");
+                Console.WriteLine($"a - b = {a - b}");
+                Console.WriteLine($"a * b = {a * b}");
+                Console.WriteLine($"a / b = {a / b}");
+            }
+            catch (Complex.NullDivision)
+            {
+                Console.WriteLine("Nullával való osztás");
+            }
+        }
+    }
+}
