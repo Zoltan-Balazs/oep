579320285def5c6c2df9dab322d0991645bfc663
Zoltán Balázs <git@zoltanbalazs.com>
2024-02-26 09:51:33 +0100

    Added 2nd practice lesson

---
+++ 2023-24-02/02/Complex/Complex.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Complex
+{
+    class Complex
+    {
+        public class NullDivision : Exception { };
+
+        private readonly double x;
+        private readonly double y;
+        public Complex(double real, double imag = 0.0)
+        {
+            x = real; y = imag;
+        }
+        public static Complex operator +(Complex a, Complex b)
+        {
+            return new Complex(a.x+b.x, a.y+b.y);
+        }
+        public static Complex operator -(Complex a, Complex b)
+        {
+            return new Complex(a.x-b.x, a.y-b.y);
+        }
+        public static Complex operator *(Complex a, Complex b)
+        {
+            return new Complex(a.x*b.x-a.y*b.y, a.x*b.y+a.y*b.x);
+        }
+        public static Complex operator /(Complex a, Complex b)
+        {
+            if (b.x==0 && b.y==0) throw new NullDivision();
+            return new Complex((a.x*b.x + a.y*b.y)/(b.x*b.x + b.y*b.y), (a.y*b.x - a.x*b.y) / (b.x*b.x + b.y*b.y));
+        }
+
+        public override string ToString()
+        {
+            if (y == 0.0) return String.Format($"{x:0.0#}");
+            else if (y > 0.0) return String.Format($"{x:0.0#}+{Math.Abs(y):0.0#}i");
+            else if (y < 0.0) return String.Format($"{x:0.0#}-{Math.Abs(y):0.0#}i");
+            return "";
+        }
+    }
+}
