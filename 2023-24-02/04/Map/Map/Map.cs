f3b05538a5d14c6b13877350c3d77beac7272b21
Zoltán Balázs <git@zoltanbalazs.com>
2024-03-04 16:47:21 +0100

    Added 4th practice lesson

--- 2023-24-02/04/Map/Map/Map.cs
+++ 2023-24-02/04/Map/Map/Map.cs
@@ -2,5 +2,133 @@
 {
     public class Map
     {
+        public class AlreadyExistingKeyException : Exception { }
+        public class NonExistingKeyException : Exception { }
+        
+        public class Item
+        {
+            public string data;
+            public int key;
+
+            // public Item(Item item)
+            // {
+            //     data = item.data;
+            //     key = item.key;
+            // }
+
+            public Item(int key, string data)
+            {
+                this.key = key;
+                this.data = data;
+            }
+
+            public override string ToString()
+            {
+                /*
+                 string s = "(";
+                 s += key.toString();
+                 s += ":";
+                 s += data;
+                 s += ")";
+                 return s;
+                 */
+                return $"({key}:{data})";
+            }
+        }
+
+        private List<Item> seq = new List<Item>();
+
+        public void SetEmpty()
+        {
+            seq.Clear();
+            // seq = new List<Item>();
+        }
+        
+        public int Count()
+        {
+            return seq.Count;
+        }
+
+        public void Insert(Item e)
+        {
+            bool l = LogSearch(e.key, out int ind);
+            if (l)
+            {
+                throw new AlreadyExistingKeyException();
+            }
+            seq.Insert(ind, e);
+        }
+
+        public void Remove(int key)
+        {
+            bool l = LogSearch(key, out int ind);
+            if (!l)
+            {
+                throw new NonExistingKeyException();
+            }
+            seq.RemoveAt(ind);
+        }
+
+        public bool In(int key)
+        {
+            // bool l = LogSearch(key, out int ind);
+            // return l;
+            return LogSearch(key, out int ind);
+        }
+
+        public string Select(int key)
+        {
+            bool l = LogSearch(key, out int ind);
+            if (!l) { 
+                throw new NonExistingKeyException();
+            }
+
+            return seq[ind].data;
+        }
+
+        private bool LogSearch(int key, out int ind)
+        {
+            bool l = false;
+            ind = -1;
+
+            int ah = 0;
+            int fh = seq.Count - 1;
+        
+            while (!l && ah <= fh)
+            {
+                ind = (ah + fh) / 2;
+
+                if (seq[ind].key > key)
+                {
+                    fh = ind - 1;
+                }
+                else if (seq[ind].key == key)
+                {
+                    l = true;
+                }
+                else if (seq[ind].key < key)
+                {
+                    ah = ind + 1;
+                }
+            }
+
+            if (!l)
+            {
+                ind = ah;
+            }
+
+            return l;
+        }
+
+        public override string ToString()
+        {
+            string s = "[";
+            foreach (Item e in seq)
+            {
+                s += e.ToString();
+            }
+            s += "]";
+            return s;
+        }
     }
 }
--- 2023-24-02/04/Map/TestMap/UnitTestMap.cs
+++ 2023-24-02/04/Map/TestMap/UnitTestMap.cs
@@ -14,9 +14,11 @@ namespace TestMap
 
             map.Insert(new Map.Map.Item(1, "barack"));
             Assert.AreEqual(map.ToString(), "[(1:barack)]");
+            Assert.AreEqual(1, map.Count());
 
             map.SetEmpty();
             Assert.AreEqual(map.ToString(), "[]");
+            Assert.AreEqual(0, map.Count());
         }
 
         [TestMethod]
@@ -35,6 +37,8 @@ namespace TestMap
             Assert.AreEqual(map.Count(), 2);
             Assert.AreEqual(map.ToString(), "[(1:barack)(2:eper)]");
             Assert.AreEqual(map.In(1), true);
+            Assert.AreNotEqual(map.In(150), true);
+            Assert.AreEqual(map.In(150), false);
 
             map.Insert(new Map.Map.Item(0, "alma"));
             Assert.AreEqual(map.Count(), 3);
@@ -45,7 +49,7 @@ namespace TestMap
             Assert.AreEqual(map.Count(), 5);
             Assert.AreEqual(map.ToString(), "[(0:alma)(1:barack)(2:eper)(4:narancs)(5:szilva)]");
 
-            Assert.ThrowsException<Map.Map.ExistingKeyException>(
+            Assert.ThrowsException<Map.Map.AlreadyExistingKeyException>(
                 () => map.Insert(new Map.Map.Item(4, "mandarin")) );
         }
 
